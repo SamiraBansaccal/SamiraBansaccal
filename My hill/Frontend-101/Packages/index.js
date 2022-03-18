829b14d1f8871cc6b0f29fb36ad23ddd197ffa4b
Samira <96707268+SamiraBansaccal@users.noreply.github.com>
2022-03-18 13:51:38 +0100

    fetch mapbox
    try to fetch the map (not working yet)

--- My hill/Frontend-101/Packages/index.js
+++ My hill/Frontend-101/Packages/index.js
@@ -9,13 +9,25 @@ document.body.appendChild(input);
 let button = document.createElement("button");
 document.body.appendChild(button);
 button.innerText = "fetch Mapbox";
+
+let div = document.createElement("div");
+document.body.appendChild(div);
+div.setAttribute("id", "map")
+
+//Fetch the coordinates
 button.addEventListener('click', (e)=>{
     let myAdress= input.value;
-    console.log(myAdress);
     let accessToken = `access_token=pk.eyJ1Ijoic2FtaXJhYmFuIiwiYSI6ImNsMHI3OGY0MTAwb2ozZHBuajE0M2lxaXUifQ.IEbMYGKgenWDrsbyM_LNOg`
     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${myAdress}.json?${accessToken}`)
     .then(response => response.json())
     .then(data => console.log(data));
 });
 
+//Add the map
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtaXJhYmFuIiwiYSI6ImNsMHI3OGY0MTAwb2ozZHBuajE0M2lxaXUifQ.IEbMYGKgenWDrsbyM_LNOg';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
 
