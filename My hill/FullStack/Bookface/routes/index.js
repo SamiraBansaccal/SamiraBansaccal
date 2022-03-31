2b7a590447cd5f22299d77dd170f206b43e2733c
Samira <96707268+SamiraBansaccal@users.noreply.github.com>
2022-03-31 19:04:01 +0200

    bookface login

--- My hill/FullStack/Bookface/routes/index.js
+++ My hill/FullStack/Bookface/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router  = express.Router();
 
-
+router.get('/dashboard',(req,res)=>{
+  res.render('dashboard',{
+    user: req.user
+    });
+})
 //login page
 router.get('/', (req,res)=>{
     res.render('welcome');
